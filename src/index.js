5ad49c5ece807ead5c809b82605a9a300110c81b
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-03-22 21:06:21 +0300

    Unit: task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr =[],
+        res = [];
+    arr = expr.split('**********');
+
+    for (let i = 0; i < arr.length; i++ ) {
+        let elment = arr[i];
+        let str_sec = '';
+        for (let j = 0; j < elment.length ; j+=10) {
+            let elment_sec = elment.slice(j, j+10);
+            let str = '';
+            for (let g = 0; g < elment_sec.length; g += 2) {
+                let compare = (elment_sec[g] + '' + elment_sec[g+1]);
+                if (compare === '10') {
+                    str += '.';
+                } else if (compare === '11') {
+                    str += '-';
+                }
+            }
+            str_sec += MORSE_TABLE[str];
+        }
+        res.push(str_sec);
+    }
+    return res.join(' ');
 }
 
 module.exports = {
